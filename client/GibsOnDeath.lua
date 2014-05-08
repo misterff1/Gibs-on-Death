ecfb0dec1a5a98671a62b5a0037543ce308a3807
misterff1 <misterff1@gmail.com>
2014-05-08 10:50:28 +0200

    Added commentary

--- client/GibsOnDeath.lua
+++ client/GibsOnDeath.lua
@@ -2,7 +2,7 @@
 
 
 function Gibsspawn(pos)
-   ClientEffect.Play(AssetLocation.Game, { effect_id = 429, position = pos, angle = Angle() })
+   ClientEffect.Play(AssetLocation.Game, { effect_id = 429, position = pos, angle = Angle() }) --Change the effect_id to something else to change the effect
 end
 
 Network:Subscribe("Gibsspawn", Gibsspawn)
