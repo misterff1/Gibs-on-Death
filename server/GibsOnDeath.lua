8c583a34ef3b2ebf0dffd7efad02c4245f02a28d
misterff1 <misterff1@gmail.com>
2014-05-08 10:51:31 +0200

    Added commentary

--- server/GibsOnDeath.lua
+++ server/GibsOnDeath.lua
@@ -3,7 +3,7 @@
 
 function PlayerDeath(args)
 	Network:SendNearby(args.player, "Gibsspawn", args.player:GetPosition())
-	Network:Send(args.player, "Gibsspawn", args.player:GetPosition())
+	Network:Send(args.player, "Gibsspawn", args.player:GetPosition()) -- Comment this line out if you experience more client crashes
 
 end
  
