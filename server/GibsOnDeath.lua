076086f6d403895ac0199bf2abe3cfe08137ad74
Misterff1 <misterff1@outlook.com>
2015-03-12 11:14:21 +0100

    Updated file to new personal standards

--- server/GibsOnDeath.lua
+++ server/GibsOnDeath.lua
@@ -1,10 +1,16 @@
--- GibsOnDeath version 1.0  |  by Misterff1
+--------------------------------------------------------------------------------------------
+----|                                Gibs on Death v1.0                                |----
+----|                                   By Misterff1                                   |----
+--------------------------------------------------------------------------------------------
 
 
-function PlayerDeath(args)
-	Network:SendNearby(args.player, "Gibsspawn", args.player:GetPosition())
-	Network:Send(args.player, "Gibsspawn", args.player:GetPosition()) -- Comment this line out if you experience more client crashes
+-- When the player dies, we want to spawn the gibs at the player's location --
+function PlayerDeath( args )
+	
+	
+	Network:SendNearby(args.player, "SpawnGibs", args.player:GetPosition())
+	Network:Send(args.player, "SpawnGibs", args.player:GetPosition()) 
 
 end
  
-Events:Subscribe("PlayerDeath", PlayerDeath)
+Events:Subscribe( "PlayerDeath", PlayerDeath )
